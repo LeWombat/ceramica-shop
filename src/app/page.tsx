3f188cc60b7037192946c3a8d09c3f16bfa642d2
Владислав Новаковский <_email@example.com>
2025-03-14 18:22:17 +0300

    Add comment to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header, Hero, About, Products, NewCollection, Footer } from '@/components'
 import styles from './page.module.scss'
 
+// Главная страница сайта керамики
 export default function Home() {
   return (
     <main className={styles.main}>
